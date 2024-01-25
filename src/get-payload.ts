1c8b05b06bd9e5a2172fe18e8e7233da6130d8cd
Geri Ramadani <ramadanigeri@gmail.com>
2024-01-25 18:11:09 +0100

    Add Media + Configure SMPT email provider + Create Verify-email page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,10 +1,21 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import payload, { Payload } from 'payload';
+import nodemailer from 'nodemailer';
 import type { InitOptions } from 'payload/config';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.resend.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'resend',
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -33,6 +44,12 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        // Add only your personal email to receive them on resend inbox during development.
+        fromAddress: 'ramadanigeri@gmail.com',
+        fromName: 'digitalNextStore',
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
